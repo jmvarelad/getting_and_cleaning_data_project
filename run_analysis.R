622d23611a9e2e42d8654025e585e3767109350d
jmvarelad <69792334+jmvarelad@users.noreply.github.com>
2022-09-08 12:03:12 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -73,3 +73,6 @@ melt_data <- melt(data_joined, id = id_labels, measure.vars = data_labels)
 
 # Apply mean function to data_joined using dcast function
 tidy_data <- dcast(melt_data, Subject + Activity_name ~ variable, mean)
+
+#Export the data set
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
